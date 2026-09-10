44fa81f20f51a41827b277f92a2aae1c6acfa9df
martaplatero <100563574@alumnos.uc3m.es>
2026-09-10 19:35:07 +0200

    update directory

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\input"
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\myoutputs"
+auxdir = r'C:/Users/marta/OneDrive/Documentos/GitHub/EODP/auxiliary'
+indir = r"C:/Users/marta/OneDrive/Desktop/MISE/2 YEAR/Earth Observation Data Processing/EODP_TER_2021/EODP-TS-L1B/input"
+outdir = r"C:/Users/marta/OneDrive/Desktop/MISE/2 YEAR/Earth Observation Data Processing/EODP_TER_2021/EODP-TS-L1B/my_output"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
