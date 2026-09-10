a4338ce2f1a6b90cc1eadebffec8f54497396b98
martaplatero <100563574@alumnos.uc3m.es>
2026-09-10 20:30:05 +0200

    Update l1b.py
    feat(l1b.py): implement restoration and equalization

--- l1b/src/l1b.py
+++ l1b/src/l1b.py
@@ -62,7 +62,8 @@ class l1b(initL1b):
         :param eq_mult: Gain factor, adimensional
         :return: TOA in DN, equalized
         """
-        #TODO
+        toa = (toa - eq_add) / eq_mult
+
         return toa
 
     def restoration(self,toa,gain):
@@ -72,7 +73,8 @@ class l1b(initL1b):
         :param gain: gain in [rad/DN]
         :return: TOA in radiances [mW/sr/m2]
         """
-        #TODO
+        toa = toa * gain
+
         self.logger.debug('Sanity check. TOA in radiances after gain application ' + str(toa[1,-1]) + ' [mW/m2/sr]')
 
         return toa
