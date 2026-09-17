79673a1463369eac2a2e82841d77bab1b2756294
martaplatero <100563574@alumnos.uc3m.es>
2026-09-17 17:57:34 +0200

    feat(mainL1b): obtain results with no equalization

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:/Users/marta/OneDrive/Documentos/GitHub/EODP/auxiliary'
-indir = r"C:/Users/marta/OneDrive/Desktop/MISE/2 YEAR/Earth Observation Data Processing/EODP_TER_2021/EODP-TS-L1B/input"
-outdir = r"C:/Users/marta/OneDrive/Desktop/MISE/2 YEAR/Earth Observation Data Processing/EODP_TER_2021/EODP-TS-L1B/my_output"
+auxdir = r'C:/Users/marta/Documents/GitHub/EODP/auxiliary'
+indir = r"C:/Users/marta/Desktop/MISE/2 YEAR/Earth Observation Data Processing/EODP_TER_2021/EODP-TS-L1B/input"
+outdir = r"C:/Users/marta/Desktop/MISE/2 YEAR/Earth Observation Data Processing/EODP_TER_2021/EODP-TS-L1B/my_output_no_equalization"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
